662f12691dc5189d92d0b46f9035d64956662bd4
Luis R. Martín <luisricardomartin@gmail.com>
2022-06-23 19:12:13 -0400

    Half stable menu

---
+++ proyecto.pas
@@ -0,0 +1,93 @@
+Program proyecto;
+uses crt;
+Var opcion: integer;
+Begin
+{
+  Realizado por:
+  Christopher Acosta - 30496179 
+  Luis Martín - 30351273
+}
+  repeat
+    clrscr;
+    writeln('--------------------------------------------------------------------------------');
+    writeln('                           SISTEMA DE MANEJO MATRICES                          ');
+    writeln('--------------------------------------------------------------------------------');
+    writeln();
+    writeln('1. LEER MATRIZ');
+    writeln('2. SISTEMA DE ECUACIONES');
+    writeln('3. MATRIZ INVERSA');
+    WriteLn('0. SALIR');
+    Writeln;
+    Write('Indique su opcion: ');
+    WriteLn;
+    writeln('--------------------------------------------------------------------------------');
+    writeln('                                 Realizado Por:                                 ');
+    writeln('          Luis Martin <30.351.273> y Christopher Acosta <30.496.179>           ');
+    writeln('--------------------------------------------------------------------------------');
+    GotoXY( 20, 10 );
+    readln(opcion);
+    Case opcion Of
+      0: exit;
+      1:begin
+        clrscr;
+        writeln('--------------------------------------------------------------------------------');
+        writeln('                           SISTEMA DE MANEJO MATRICES                          ');
+        writeln('                             SUB MENU 1. LEER MATRIZ                            ');
+        writeln('--------------------------------------------------------------------------------');
+        Writeln;
+        writeln('1.1 LEER MATRIZ DESDE TECLADO');
+        writeln('1.2 LEER MATRIZ DESDE ARCHIVO');
+        writeln('1.3 MOSTRAR MATRIZ');
+        writeln('1.4 MODIFICAR ELEMENTOS EN LA MATRIZ MANUALMENTE');
+        writeln('1.5 ALMACENAR MATRIZ EN ARCHIVO');
+        WriteLn('0. VOLVER AL MENU ANTERIOR');
+        WriteLn;
+        Writeln('Indique su opcion: ');
+        WriteLn;
+        writeln('--------------------------------------------------------------------------------');
+        GotoXY( 20, 13 );
+        readln(opcion);
+      end;
+      2:begin
+        clrscr;
+        writeln('--------------------------------------------------------------------------------');
+        writeln('                           SISTEMA DE MANEJO MATRICES                          ');
+        writeln('                    SUB MENU 2. RESOLVER SISTEMA DE ECUACIONES                  ');
+        writeln('--------------------------------------------------------------------------------');
+        Writeln;
+        writeln('2.1 LEER VECTOR DE TERMINOS INDEPENDIENTES DEL SISTEMA');
+        writeln('2.2 RESOLVER Y MOSTRAR SOLUCIÓN DEL SISTEMA');
+        writeln('2.3 MOSTRAR MATRIZ Y VECTOR ADJUNTO');
+        writeln('2.4 MODIFICAR MATRIZ Y VECTOR ADJUNTO MANUALMENTE');
+        writeln('2.5 GUARDAR EN ARCHIVO EL SISTEMA JUNTO CON LA SOLUCION');
+        WriteLn('0. VOLVER AL MENU ANTERIOR');
+        WriteLn;
+        Writeln('Indique su opcion: ');
+        WriteLn;
+        writeln('--------------------------------------------------------------------------------');
+        GotoXY( 20, 13 );
+        readln(opcion);
+      end;
+      3:begin
+        clrscr;
+        writeln('--------------------------------------------------------------------------------');
+        writeln('                           SISTEMA DE MANEJO MATRICES                          ');
+        writeln('                           SUB MENU 3. MATRIZ INVERSA                           ');
+        writeln('--------------------------------------------------------------------------------');
+        Writeln;
+        writeln('3.1 HALLAR MATRIZ INVERSA');
+        writeln('3.2 MOSTAR MATRIZ INVERSA');
+        writeln('3.3 COMPROBAR MATRIZ INVERSA');
+        writeln('3.4 GUARDAR EN ARCHIVO MATRIZ ORIGINAL E INVERSA');
+        WriteLn('0. VOLVER AL MENU ANTERIOR');
+        WriteLn;
+        Writeln('Indique su opcion: ');
+        WriteLn;
+        writeln('--------------------------------------------------------------------------------');
+        GotoXY( 20, 12 );
+        readln(opcion);
+      end;
+    End;
+    Until opcion = 0;
+    ClrScr;
+End.
